b899f5c5610ee167ed1e24db9cb8c2621f936780
alexismouwid <alexismouwid@gmail.com>
2025-04-03 11:20:48 -0500

    mod en server.js y en vercel

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 dotenv.config();
 const app = express ();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json()); //para manejar json
 app.use('/reservas', reservasRouter);
 
